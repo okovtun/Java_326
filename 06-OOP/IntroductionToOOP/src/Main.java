cfad0641a34f2030fb2a9574c760482dc3590f08
Clayman <Clayman@DESKTOP-DM8E77U>
2023-09-26 21:26:47 +0300

    Introduction to OOP

---
+++ 06-OOP/IntroductionToOOP/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        int a;                  //Объявление переменной 'a' типа 'int'
+        Point A = new Point();  //Объявление переменной 'A' типа 'Point'
+                                //Создание объекта (экземпляра) 'A' класса 'Point'
+        A.x = 2;
+        A.y = 3;
+        System.out.println("X = " + A.x);
+        System.out.println("Y = " + A.y);
+        //Object
+    }
+}
+class Point
+{
+    double x;
+    double y;
+}
