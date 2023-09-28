17db10c1bedfc593945debc295b74389ec80d927
Clayman <Clayman@DESKTOP-DM8E77U>
2023-09-28 21:32:09 +0300

    Encapsulation in Point DONE

--- 06-OOP/IntroductionToOOP/src/Main.java
+++ 06-OOP/IntroductionToOOP/src/Main.java
@@ -5,10 +5,12 @@ public class Main {
         int a;                  //Объявление переменной 'a' типа 'int'
         Point A = new Point();  //Объявление переменной 'A' типа 'Point'
                                 //Создание объекта (экземпляра) 'A' класса 'Point'
-        A.x = 2;
-        A.y = 3;
-        System.out.println("X = " + A.x);
-        System.out.println("Y = " + A.y);
+        //A.x = 2;
+        //A.y = 3;
+        A.setX(2);
+        A.setY(3);
+        System.out.println("X = " + A.getX());
+        System.out.println("Y = " + A.getY());
         //Object
     }
 }
@@ -16,4 +18,20 @@ class Point
 {
     private double x;
     private double y;
+    public double getX()
+    {
+        return x;
+    }
+    public double getY()
+    {
+        return y;
+    }
+    public void setX(double x)
+    {
+        this.x = x;
+    }
+    public void setY(double y)
+    {
+        this.y = y;
+    }
 }
