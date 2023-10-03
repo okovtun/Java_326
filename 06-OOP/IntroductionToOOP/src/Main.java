176d3b11fc103a50838ab018b792986bd1fc0b06
Clayman <Clayman@DESKTOP-DM8E77U>
2023-10-03 21:23:52 +0300

    distance DONE

--- 06-OOP/IntroductionToOOP/src/Main.java
+++ 06-OOP/IntroductionToOOP/src/Main.java
@@ -9,9 +9,21 @@ public class Main {
         //A.y = 3;
         A.setX(2);
         A.setY(3);
-        System.out.println("X = " + A.getX());
-        System.out.println("Y = " + A.getY());
+//        System.out.println("X = " + A.getX());
+//        System.out.println("Y = " + A.getY());
         //Object
+        A.print();
+
+        Point B = new Point();
+        B.setX(7);
+        B.setY(8);
+        //System.out.println("X = %f, Y = %f".formatted(B.getX(), B.getY()));
+        B.print();
+
+        System.out.println("Расстояние от точки 'A' до точки 'B': %f".formatted(A.distance(B)));
+        System.out.println("Расстояние от точки 'B' до точки 'A': %f".formatted(B.distance(A)));
+        System.out.println("Расстояние между точками 'A' и 'B': %f".formatted(Point.distance(A, B)));
+        System.out.println("Расстояние между точками 'B' и 'A': %f".formatted(Point.distance(B, A)));
     }
 }
 class Point
@@ -34,4 +46,36 @@ class Point
     {
         this.y = y;
     }
+    public double distance(Point other)
+    {
+        //this
+        //other
+        double x_distance = this.x - other.x;
+        double y_distance = this.y - other.y;
+        double distance = Math.sqrt(x_distance*x_distance + y_distance*y_distance);
+        //Math - это встроенный класс, который содержит математические методы (функции)
+        //SQRT - Square Root (Квадратный корень)
+        return distance;
+    }
+    public static double distance(Point A, Point B)
+    {
+        /*double x_distance = A.x - B.x;
+        double y_distance = A.y - B.y;
+        double distance = Math.sqrt(x_distance*x_distance + y_distance*y_distance);
+        return distance;*/
+        return Math.sqrt(Math.pow(A.x - B.x, 2) + Math.pow(A.y - B.y, 2));
+    }
+    public void print()
+    {
+        System.out.println("X = %f, Y = %f".formatted(x, y));
+    }
 }
+
+/*
+type name ( parameters )
+{
+    ........;
+    group-of-statements;
+    ........;
+}
+*/
